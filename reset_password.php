0341e507126e938cc9de95e04cc6f270fd18abfb
Toby Anderson <toby.anderson@gmail.com>
2012-06-08 14:18:32 +1000

    Moved a function for validating an email address from the reset_password.php to common.php, where it belongs. It'll be useful for the rego form.

--- reset_password.php
+++ reset_password.php
@@ -6,7 +6,7 @@
  */
 // modified by Toby Anderson
 
-require_once 'common.php';
+require_once '../php/db.php';
 
 //this function will display error messages in alert boxes, used for login forms so if a field is invalid it will still keep the info
 //use error('foobar');
@@ -26,35 +26,6 @@ function error($msg) {
     </html>
     <?
     exit;
-}
-
-//This functions checks and makes sure the email address that is being added to database is valid in format. 
-function check_email_address($email) {
-  // First, we check that there's one @ symbol, and that the lengths are right
-  if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
-    // Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
-    return false;
-  }
-  // Split it into sections to make life easier
-  $email_array = explode("@", $email);
-  $local_array = explode(".", $email_array[0]);
-  for ($i = 0; $i < sizeof($local_array); $i++) {
-     if (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
-      return false;
-    }
-  }  
-  if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) { // Check if domain is IP. If not, it should be valid domain name
-    $domain_array = explode(".", $email_array[1]);
-    if (sizeof($domain_array) < 2) {
-        return false; // Not enough parts to domain
-    }
-    for ($i = 0; $i < sizeof($domain_array); $i++) {
-      if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
-        return false;
-      }
-    }
-  }
-  return true;
 }
 
 
