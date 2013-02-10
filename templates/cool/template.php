242b76d5400622a1a8676c0a6ce0b1680700bdd3
Toby Anderson <toby.anderson@gmail.com>
2013-02-10 23:16:20 +0530

    Began to reorganise how the templates are processed. The process needs to be completely generic so new templates can be easily made. Each container of content has a beginning, an ending and possible there are between bits.

--- templates/cool/template.php
+++ templates/cool/template.php
@@ -41,7 +41,270 @@ $hr = <<<HOZRULE
 
 HOZRULE;
 
+$template['web']['begin']['header'] = <<<WEBHEADERBEGIN
+            <!--header text-->
+            <table style="background-image: url(<!--TEMPLATE IMAGE PATH-->header-lines.jpg); background-position: bottom; background-repeat: repeat-x;" bgcolor="#1d3952" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td>
+                        <table id="top" width="578" border="0" align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td valign="top" align="center" height="60">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td height="10"></td>
+                                        </tr>
+                                    </table>
+WEBHEADERBEGIN;
 
+$template['web']['end']['header'] = <<<WEBHEADEREND
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/header text-->
+WEBHEADEREND;
+
+$template['web']['begin']['headerText'] = '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 12px; color: #4484b5; margin: 0; padding: 0;">';
+$template['web']['end']['headerText'] = '</p>';
+$template['web']['between']['headerText']['headerText'] = "\n";
+
+$template['web']['begin']['footer'] = <<<WEBFOOTERBEGIN
+            <!--footer-->
+            <table style="background-image: url(<!--TEMPLATE IMAGE PATH-->footer-lines.jpg); background-position: top; background-repeat: repeat-x;" bgcolor="#1c293b" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td>
+                        <table width="578" border="0" align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td valign="top" align="center" height="60">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td height="15"></td>
+                                        </tr>
+                                    </table>
+WEBFOOTERBEGIN;
+
+$template['web']['end']['footer'] = <<<WEBFOOTEREND
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/footer-->
+WEBFOOTEREND;
+
+$template['web']['begin']['footerText'] = '<p style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; margin: 0px; padding: 0px; color: #30648d; text-shadow: 1px 1px 1px #000;">';
+$template['web']['end']['footerText'] = '</p>';
+$template['web']['between']['footerText']['footerText'] = "\n";
+
+
+$template['web']['begin']['newsletter'] = <<<WEBNEWSLETTERBEGIN
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><!--TITLE_ONSET--> <!--TITLE_CODA--> - Issue <!--NUM--></title>
+<!--[if gte mso 9]>
+<style type="text/css">
+.body { background: #234865 url('<!--TEMPLATE IMAGE PATH-->body-bg.jpg'); }	     
+.case { background: none; }
+</style>
+<![endif]-->
+</head>
+<body class="body" style="margin: 0; padding: 0; background-color: #234865; background-image: url(<!--TEMPLATE IMAGE PATH-->body-bg.jpg); background-repeat: repeat;" marginheight="0" topmargin="0" marginwidth="0" leftmargin="0">
+<!--100% body table-->
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+        <td class="case" bgcolor="#234865" style="background-color: #234865; background-image: url(<!--TEMPLATE IMAGE PATH-->body-bg.jpg); background-repeat: repeat;">
+            <!--[if gte mso 9]>
+<td style="background: none;">
+<![endif]-->
+            <!--HEADER-->
+            <!--intro-->
+            <table width="578" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td valign="top" height="20"></td>
+                </tr>
+                <tr>
+                    <td background="<!--TEMPLATE IMAGE PATH-->header-bg.jpg" bgcolor="#c6d2db" valign="top" height="79">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td height="22" width="68%"></td>
+                                <td height="20" width="32%"></td>
+                            </tr>
+                            <tr>
+                                <td valign="top">
+                                    <table width="82%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td align="center" valign="bottom" width="7%">
+                                                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                    <tr>
+                                                        <td width="15"></td>
+                                                        <td width="10"></td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td valign="middle" width="93%">
+                                                <h1 style="font-family: Helvetica, Arial, sans-serif; font-size: 24px; margin: 0px; padding: 0px; text-shadow: 1px 1px 1px #FFFFFF; color: #244a67"><!--TITLE_ONSET--><span style=" font-weight: normal;"><!--TITLE_CODA--></span></h1>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td valign="top">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td height="10"></td>
+                                        </tr>
+                                    </table>
+                                    <p style=" font-family:Helvetica, Arial, sans-serif; font-weight: bold; font-size: 12px; margin: 0px; padding: 0px; text-shadow: 1px 1px 1px #FFFFFF; color: #336791">
+                                        Issue <!--NUM--> - <!--DATE-->
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" height="20"></td>
+                </tr>
+            </table>
+            <!--/intro-->
+            <!--content section-->
+            <table width="576" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+WEBNEWSLETTERBEGIN;
+
+$template['web']['end']['newsletter'] = <<<WEBNEWSLETTEREND
+</tr>
+            </table>
+            <!--break-->
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td height="25"></td>
+                </tr>
+            </table>
+            <!--/break-->
+            <!--FOOTER-->
+        </td>
+    </tr>
+</table>
+<!--/100% body table-->
+</body>
+</html>
+WEBNEWSLETTEREND;
+
+$template['web']['begin']['main'] = <<<WEBMAINBEGIN
+                    <td width="350" valign="top">
+                        <table width="100%" border="0" cellpadding="20" cellspacing="0">
+                            <tr>
+                                <td valign="top" bgcolor="#1c3851" background="<!--TEMPLATE IMAGE PATH-->content-bg.jpg" style="border: solid 1px #193044; border-radius: 8px; -moz-border-radius: 8px; -webkit-border-radius: 8px; -khtml-border-radius: 8px;">
+WEBMAINBEGIN;
+
+$template['web']['end']['main'] = <<<WEBMAINEND
+                                </td>
+                            </tr>
+                        </table>
+                        <!--break-->
+                    </td>
+WEBMAINEND;
+
+$template['web']['begin']['secondary'] = '<td valign="top">';
+$template['web']['end']['secondary'] ='</td>';
+
+$template['web']['begin']['mainArticle'] = '<h2 style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; margin: 0px; padding: 0px; text-shadow: 1px 1px 1px #333333; color: #62b6ee;"><!--MAIN ARTICLE TITLE--></h2>';
+$template['web']['end']['mainArticle'] = "\n";
+$template['web']['between']['mainArticle']['mainArticle'] = $break . $hr . $break;
+
+$template['web']['begin']['secondaryArticle'] = <<<SECONDARYARTICLEBEGIN
+
+                        <!--section-->
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td width="21">&nbsp;</td>
+                                            <td>
+                                                <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                                                    <tr>
+                                                        <td height="17" width="204" valign="top" bgcolor="#1c3851" background="<!--TEMPLATE IMAGE PATH-->content-bg.jpg" style="border: solid 1px #193044; 
+					-webkit-border-top-left-radius: 8px;
+	                -khtml-border-radius-topleft: 8px;	
+	                -moz-border-radius-topleft: 8px;
+	                border-top-left-radius: 8px;
+					-webkit-border-top-right-radius: 8px;
+	                -khtml-border-radius-topright: 8px;	
+	                -moz-border-radius-topright: 8px;
+	                border-top-right-radius: 8px;
+				    border-bottom: none;"> </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td width="19">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td height="38" align="center">
+                                                <table width="202" border="0" cellspacing="0" cellpadding="0">
+                                                    <tr>
+                                                        <td valign="top" style="background-image: url(<!--TEMPLATE IMAGE PATH-->ribbon.jpg); background-repeat: no-repeat; background-position: center;" bgcolor="#2b7ab7" height="38">
+                                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                                <tr>
+                                                                    <td width="10"></td>
+                                                                    <td height="6"></td>
+                                                                    <td height="6"></td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>&nbsp;</td>
+                                                                    <td>
+                                                                        <p style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; margin-top: 0px; margin-bottom: 0px; padding: 0px; color: #1f2d41; font-size: 16px; font-weight: bold; text-shadow: 0px 1px 1px #ffffff"><!--SECONDARY ARTICLE TITLE--></p>
+                                                                    </td>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td width="20">&nbsp;</td>
+                                            <td>
+                                                <table width="100%" border="0" cellpadding="20" cellspacing="0">
+                                                    <tr>
+                                                        <td width="204" valign="top" bgcolor="#1c3851" background="<!--TEMPLATE IMAGE PATH-->content-bg.jpg" style="border: solid 1px #193044; 
+					-webkit-border-bottom-left-radius: 8px;
+	                -khtml-border-radius-bottomleft: 8px;	
+	                -moz-border-radius-bottomleft: 8px;
+	                border-bottom-left-radius: 8px;
+					-webkit-border-bottom-right-radius: 8px;
+	                -khtml-border-radius-bottomright: 8px;	
+	                -moz-border-radius-bottomright: 8px;
+	                border-bottom-right-radius: 8px;
+				    border-top: none;">
+SECONDARYARTICLEBEGIN;
+
+$template['web']['end']['secondaryArticle'] = <<<SECONDARYARTICLEEND
+                                    
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td width="19">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                        <!--/section-->
+                        
+SECONDARYARTICLEEND;
+
+$template['web']['between']['secondaryArticle']['secondaryArticle'] = $bigBreak;
 
 $templateData['web']['file'] = <<<WEBFILE
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
