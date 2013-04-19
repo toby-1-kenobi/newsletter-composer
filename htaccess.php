b5a7c9042d468ce883a452da52776a7fa5883a6f
Toby Anderson <toby.anderson@gmail.com>
2013-04-19 22:21:47 +0530

    Beginnings of a script to write the .htaccess file

---
+++ htaccess.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'common.php';
+
+if (login_ok() == 1) {
+	
+	$rel_user_path = 'users/' . $_SESSION['uid'] . '/';
+	
+	$htaccess_content = '<FilesMatch "\.(html|htm)$">';
+	$htaccess_content .= "\nAuthType Basic";
+	$htaccess_content .= "\nAuthName \"Access to this file is restricted\"";
+	$htaccess_content .= "\nAuthUserFile {$_ENV['DOCUMENT_ROOT']}/$rel_user_path.htpasswd";
+	$htaccess_content .= "\nrequire valid-user";
+	$htaccess_content .= "\n</FilesMatch>";
+
+} else {
+	echo '<p>.htaccess not written. Could not verify user. Login again.</p>';
+}
